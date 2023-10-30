2fdbfbcea33678529b1c0dedee71814334e2f1b9
Jonathan Luembe <jonathanluembe@yahoo.com>
2023-10-31 00:12:37 +0100

    jsx

---
+++ src/components/EditTodoForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function EditodoForm({editTodo, task}) {
+  const [value, setValue] = useState(task.task);
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("")
+  };
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="Modifiez la tache"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Modifier la tache
+      </button>
+    </form>
+  );
+}
